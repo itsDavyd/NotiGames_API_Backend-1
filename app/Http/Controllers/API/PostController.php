d36ab37e47ec83e18863b7c34abcb3ffb2c67117
Jose Chavez Guevara <jose.chavez@tokapp.com>
2022-04-13 14:42:50 +0200

    Primer commit de la APIrest de NotiGames

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    //
+    public function getAll()
+    {
+        $data = Post::get();
+        
+        return response()->json($data,200);
+    }
+
+    public function create(Request $request)
+    {
+        $data['titulo'] = $request['titulo'];
+        $data['texto'] = $request['texto'];
+        $data['fechaPost'] = $request['fechaPost'];
+        $data['idUsuario'] = $request['idUsuario'];
+        $data['idCategoria'] = $request['idCategoria'];
+        Post::create($data);
+
+        return response()->json([
+            'message' => "Post creado Correctamente",
+            'success' => true
+        ], 200);
+    }
+
+    public function delete($id)
+    {
+        $res = Post::find($id)->delete();
+
+        return response()->json([
+            'message' => "Post borrado Correctamente",
+            'success' => true
+        ], 200);
+    }
+
+    public function get($id){
+        $data = Post::find($id);
+
+        return response()->json($data, 200);
+    }
+
+    public function update(Request $request,$id){
+        $data['titulo'] = $request['titulo'];
+        $data['texto'] = $request['texto'];
+        // $data['fechaPost'] = $request['fechaPost'];
+        $data['idUsuario'] = $request['idUsuario'];
+        $data['idCategoria'] = $request['idCategoria'];
+        Person::find($id)->update($data);
+
+        return response()->json([
+            'message' => "Post actualizado Correctamente",
+            'success' => true
+        ], 200);
+    }
+}
---
+++ database/migrations/2022_04_13_120051_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo');
+            $table->string('texto');
+            $table->timestamp('fechaPost')->useCurrent();
+            $table->unsignedBigInteger('idUsuario');
+            $table->foreign('idUsuario')->references('id')->on('users')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+            $table->unsignedBigInteger('idCategoria');
+            $table->foreign('idCategoria')->references('id')->on('categorias')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
